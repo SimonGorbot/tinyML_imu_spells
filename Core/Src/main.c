9de15c1e2cf2c522fda966547005446d78fa0682
Nathan Li <nathan.li1@uwaterloo.ca>
2025-11-24 14:15:03 -0500

    Fixed MSB shift bug

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -89,6 +89,9 @@ int32_t platform_write(void *handle, uint8_t reg,
 int32_t platform_read(void *handle, uint8_t reg,
                       uint8_t *bufp, uint16_t len)
 {
+  if (len > 1) {
+        reg |= 0x80; // Set MSB for address auto-increment
+    }
   if (HAL_I2C_Mem_Read(&hi2c1,
                        LIS3DH_ADDR,
                        reg,
